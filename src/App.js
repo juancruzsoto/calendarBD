a1eb4b4c1dd67a12a941a9f255072b890bb0c74d
Juan Cruz Soto <juancruzlp2013@gmail.com>
2021-08-30 14:10:51 -0300

    Login view created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MenuLogin from "./components/MenuLogin";
+
+function App() {
+  return (
+    <div className="App">
+     <MenuLogin/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/jss/menuStyle.js
@@ -0,0 +1,30 @@
+const menuStyle = {
+	root: {
+    flexGrow: 1,
+    textAlign: 'center',
+    minHeight: 400,
+    width: '100%',
+    marginTop: '60px'
+  },
+  menucard:{
+    backgroundColor: "#333333",
+  },
+  container:{
+    padding: '20px',
+    marginTop: '30px',
+  },
+  content:{
+    height: 100,
+    paddingBottom: "2rem"
+  },
+  
+  title:{
+    color: "#f03030",
+    fontWeight: 400
+  },
+  media: {
+    height: 250,
+  },
+};
+
+export default menuStyle;
